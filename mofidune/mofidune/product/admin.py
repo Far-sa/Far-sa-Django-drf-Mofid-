1f67ea895a3546f76445a6dd38f7d6da367a14f7
Far-sa <fmohtasham82@gmail.com>
2023-01-10 21:16:13 +0300

    join table-left join

--- mofidune/mofidune/product/admin.py
+++ mofidune/mofidune/product/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 
-from .models import Brand, Category, Product
+from .models import Brand, Category, Product, ProductLine
 
 # Register your models here.
 
-admin.site.register(Product)
+
+class ProductLineInline(admin.TabularInline):
+    model = ProductLine
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductLineInline]
+
+
+# admin.site.register(Product,ProductAdmin)
 admin.site.register(Brand)
 admin.site.register(Category)
+admin.site.register(ProductLine)
--- mofidune/mofidune/product/serializers.py
+++ mofidune/mofidune/product/serializers.py
@@ -4,29 +4,46 @@ from .models import Brand, Category, Product, ProductLine
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    category_name = serializers.CharField(source="name")
+
     class Meta:
         model = Category
-        fields = ["name"]
+        fields = ["category_name"]
 
 
 class BrandSerializer(serializers.ModelSerializer):
     class Meta:
         model = Brand
-        fields = "__all__"
-
-
-class ProductSerializer(serializers.ModelSerializer):
-    brand = BrandSerializer()
-    category = CategorySerializer()
-
-    class Meta:
-        model = Product
-        fields = "__all__"
+        exclude = ("id",)
 
 
 class ProductLineSerializer(serializers.ModelSerializer):
-    Product = ProductSerializer()
+    # Product = ProductSerializer()
 
     class Meta:
         model = ProductLine
-        fields = "__all__"
+        exclude = (
+            "id",
+            "product",
+        )
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    # brand = BrandSerializer()
+    brand_name = serializers.CharField(source="brand.name")
+    # category = CategorySerializer()
+    category_name = serializers.CharField(source="category.name")
+    product_line = ProductLineSerializer(many=True)
+
+    class Meta:
+        model = Product
+        fields = [
+            "name",
+            "slug",
+            "description",
+            # "brand",
+            "brand_name",
+            # "category",
+            "category_name",
+            "product_line",
+        ]
